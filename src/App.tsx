7c25c0a6cf0bb357e0e413357b615f2c808f87e1
Shivam Kulkarni <shivam.suyash@gmail.com>
2026-01-24 23:44:10 +0530

    refactor: theme context

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import Layout from "./pages/Layout";
 import ContactUs from "./pages/ContactUs";
 import AboutUs from "./pages/AboutUs";
 import Home from "./pages/Home";
+import ThemeProvider from "./context/ThemeProvider";
 
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-        </Route>
-      </Routes>
-		</BrowserRouter>
+		<ThemeProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<Layout />}>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<AboutUs />} />
+						<Route path="/contact" element={<ContactUs />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
 	);
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,22 +1,21 @@
 import { Outlet } from "react-router";
 import Footer from "../components/Footer";
 import Navbar from "../components/NavBar";
-import ThemeProvider from "../context/ThemeProvider";
 
 export default function Layout() {
 	return (
-		<ThemeProvider>
-			<div className="app-container min-h-screen flex flex-col">
-				<header className="sticky top-0 z-50">
-					<Navbar />
-				</header>
+		<div className="app-container min-h-screen flex flex-col">
+			<header className="sticky top-0 z-50">
+				<Navbar />
+			</header>
 
-				<main className="grow m-5"><Outlet /></main>
+			<main className="grow m-5">
+				<Outlet />
+			</main>
 
-				<footer>
-					<Footer />
-				</footer>
-			</div>
-		</ThemeProvider>
+			<footer>
+				<Footer />
+			</footer>
+		</div>
 	);
 }
