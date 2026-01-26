76693e40771fe0bb5b018e7f28ca00264e311ec8
Shivam Kulkarni <shivam.suyash@gmail.com>
2026-01-26 19:06:10 +0530

    chore: position the notification

--- src/components/NotificationContainer.tsx
+++ src/components/NotificationContainer.tsx
@@ -2,15 +2,23 @@ import { useAppSelector } from "../store/hooks";
 import Toast from "./Notification";
 
 export default function ToastContainer() {
-  const notifications = useAppSelector((state) => state.notification.items);
+	const notifications = useAppSelector((state) => state.notification.items);
 
-  return (
-    <div className="fixed top-20 right-5 z-50 flex flex-col gap-3 pointer-events-none">
-      {notifications.map((notification) => (
-        <div key={notification.id} className="pointer-events-auto">
-          <Toast {...notification} />
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div
+			className="
+        fixed z-40 flex flex-col gap-3 pointer-events-none
+        bottom-5 left-1/2 -translate-x-1/2
+        w-[92vw] max-w-md
+        sm:left-auto sm:translate-x-0 sm:right-5
+        sm:w-auto sm:max-w-none
+      "
+		>
+			{notifications.map((notification) => (
+				<div key={notification.id} className="pointer-events-auto">
+					<Toast {...notification} />
+				</div>
+			))}
+		</div>
+	);
 }
